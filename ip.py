c03696799cd5508927f14becd277df81d092f4b1
DhruvMitna19 <dhruvmitna9@gmail.com>
2021-10-06 12:45:49 +0530

    initial commit

---
+++ ip.py
@@ -0,0 +1,11 @@
+from socket import gethostbyname, gaierror
+
+host = input("Enter the website that you wannt to trace the ip of: ")
+
+try:
+
+    print(f"The IP address is {gethostbyname(host)}")
+
+except gaierror:
+
+    print("Please enter a valid domain.")
