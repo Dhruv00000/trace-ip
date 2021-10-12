e4268b52be20f0c9865c1568f6292bd1edc060da
DhruvMitna19 <dhruvmitna9@gmail.com>
2021-10-12 11:34:33 +0530

    added comments

--- ip.py
+++ ip.py
@@ -1,11 +1,15 @@
+# Importing the necessary functions from the module.
 from socket import gethostbyname, gaierror
 
+# Getting the user input.
 host = input("Enter the website that you wannt to trace the ip of: ")
 
 try:
 
+    # Finding and printing the ip.
     print(f"The IP address is {gethostbyname(host)}")
 
+# If the user enters an invalid domain, this message is printed out instead of the program crashing with an error.
 except gaierror:
 
     print("Please enter a valid domain.")
